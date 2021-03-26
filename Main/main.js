aedf5d9f03d3aba87b9d74dea87600f9b3e984e4
grey cashmere <dobsonemily@gmail.com>
2021-03-26 16:57:49 -0700

    adding questions for query

---
+++ Main/main.js
@@ -0,0 +1,60 @@
+
+Const questions = [
+   
+    {type: "list",
+    name: "action",
+    message: "What would you like to do?", 
+    choices: [
+    "View All Employees ", 
+    "View All Employees By Department", 
+    "Add Employee",
+    "Remove Employee",
+    "Update Employee Role",
+    "Update Employee Manager"]
+    },
+    {
+    type: "input",
+    name: "role ",
+    message: "What is the name of the role?"
+    },
+    {
+        type: "input",
+        name: "salary ",
+        message: "What is salary of the role?"
+    },
+    {
+        type: "input",
+        name: "department ",
+        message: "Which department does the role belong to?"
+    },
+    {
+        type: "input",
+        name: "nameofDept ",
+        message: "What is the name of the Department?"
+    },
+    {
+        type: "input",
+        name: "firstName ",
+        message: "What is the first name of the Employee?"
+    },
+    {
+        type: "input",
+        name: "lastName ",
+        message: "What is the last name of the Employee?"
+    },
+    {
+        type: "List",
+        name: "manager",
+        message: "Who is the Employees manager?"
+    },
+    
+
+
+]; 
+
+
+
+
+
+; 
+
