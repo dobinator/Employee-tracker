770bf9ac13efedaaf4cf1a8e52c4e7dc44cc3c14
grey cashmere <dobsonemily@gmail.com>
2021-03-30 22:10:25 -0700

    adding to index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql");
 
-
 const connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -29,7 +28,7 @@ function init() {
           "View All Roles",
           "Add An Employee",
           "Add a Department",
-          "Add a Roles",
+          "Add a Role",
           "Remove Employee",
           "Delete Roles",
           "Update Employee Roles",
@@ -56,8 +55,8 @@ function init() {
         case "Add a Department":
           addADepartment();
           break;
-        case "Add Roles":
-          addRoles();
+        case "Add a Role":
+          addARole();
           break;
         case "Remove An Employee":
           removeEmployee();
@@ -78,31 +77,28 @@ function init() {
           connection.end();
       }
     });
-};
+}
 
 function viewAllEmployees() {
-  connection.query(
-    "SELECT * FROM employees", function (err, data) {
+  connection.query("SELECT * FROM employees", function (err, data) {
     console.table(data);
     init();
   });
-};
+}
 
 function viewDepartments() {
-  connection.query(
-    "SELECT * FROM department", function (err, data) {
+  connection.query("SELECT * FROM department", function (err, data) {
     console.table(data);
     init();
   });
-};
+}
 
 function viewAllRoles() {
-  connection.query(
-    "SELECT * FROM roles", function (err, data) {
+  connection.query("SELECT * FROM roles", function (err, data) {
     console.table(data);
     init();
   });
-};
+}
 
 function addAnEmployee() {
   inquirer
@@ -127,69 +123,107 @@ function addAnEmployee() {
         type: "list",
         name: "manager_id",
         message: "What is the Employee Manager's id?",
-        choices: [1, 2, 3, 4], 
+        choices: [1, 2, 3, 4],
       },
     ])
     .then(function (response) {
       console.log(response);
-      const query = connection.query(
-        "INSERT INTO employees ( first_name,last_name,role_id, manager_id) VALUES (?, ?, ?, ?);", 
-       
-        [
-          response.first_name,
-          response.last_name,
-          response.role_id,
-          response.manager_id,
-        ],
+      connection.query("INSERT INTO employees SET ?", {
+        first_name: response.first_name,
+        last_name: response.last_name,
+        role_id: response.role_id,
+        manager_id: response.manager_id,
+      }),
         (err, data) => {
           if (err) throw err;
           console.log(
             "Added Employee",
             response.first_name,
-            response.last_name,
-
+            response.last_name
           );
 
           init();
-        }
-      );
+        };
     });
-    
 }
 
-function addADepartment(){
-  inquirer.prompt([
-    {
-    type: "input",
-    name: "department",
-    message: "Please add a name of the department you would like to add"
-  },
-  {
-    type: "input",
-    name: "department manager",
-    message: "Please add the name of the department manager you would like to add"
-  },
+function addADepartment() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "department_name",
+        message: "Please add a name of the department you would like to add",
+      },
+      {
+        type: "input",
+        name: "department_manager",
+        message:
+          "Please add the name of the department manager you would like to add",
+      },
+    ])
+    .then(function (response) {
+      console.log(response);
+      connection.query("INSERT INTO department SET ?", {
+        department_name: response.department_name,
+        department_manager: response.department_manager,
+      }),
+        (err, data) => {
+          if (err) throw err;
+          console.log(
+            "Added Department",
+            response.department_name,
+            response.department_manager
+          );
 
-  ]).then (function (response){
-    console.log (response);
-    const query= 
-    "INSERT INTO department (department_name, department_manager) VALUES (?,?):";
-    const addDept = connection.query(
-      query,
-      [
-        response.department_name,
-        response.department_manager,
-       
-      ],
-      function (err, data){
-        console.log(
-          "Added department",
-          response.department_name,
-          response.adddepartment_manager,
-        );
+          init();
+        };
+    
+    });
+}
+function addARole() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "title",
+        message: "What is the name of the title you would like to add?",
+      },
+      {
+        type: "input",
+        name: "salary",
+        message: "What is the salary amount?",
+      },
+      {
+        type: "list",
+        name: "department_id",
+        message: "What is the number that corresponds to the department id?",
+        choices: [1, 2, 3, 4, 5],
+      },
+    ])
+    .then(function (response) {
+      console.log(response);
+      connection.query("INSERT INTO roles SET ?", {
+        title: response.title,
+        salary: response.salary,
+        department_id: response.department_id,
+      }),
+        (err, data) => {
+          if (err) throw err;
+          console.log(
+            "Added Role",
+            response.title,
+            response.salary,
+            response.department_id
+          );
 
-        init();
-        }
-  
-  )})}; 
+          init();
+        };
+    });
+}
 
+function removeEmployee(){
+ inquirer 
+
+
+}
